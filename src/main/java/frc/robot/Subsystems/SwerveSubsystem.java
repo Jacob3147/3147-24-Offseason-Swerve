71a6db068152dce70360067b9dbc8760ceba6a7f
Jacob3147 <schnitz1986@gmail.com>
2024-05-05 17:50:27 -0500

    .

--- src/main/java/frc/robot/Subsystems/SwerveSubsystem.java
+++ src/main/java/frc/robot/Subsystems/SwerveSubsystem.java
@@ -26,6 +26,7 @@ import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.util.LimelightPose;
+import frc.robot.util.PhotonPose;
 import swervelib.SwerveDrive;
 import swervelib.parser.SwerveParser;
 
@@ -267,6 +268,9 @@ public class SwerveSubsystem extends SubsystemBase
     @Override
     public void periodic() 
     {
+        LimelightPose.evaluate(getPoseEstimator.get(), speedsFieldRelative.get(), field);
+        //PhotonPose.evaluate(getPoseEstimator.get(), speedsFieldRelative.get(), field);
+
         field.setRobotPose(poseSupplier.get());
     }
 
--- src/main/java/frc/robot/util/LimelightPose.java
+++ src/main/java/frc/robot/util/LimelightPose.java
@@ -2,14 +2,17 @@ package frc.robot.util;
 
 import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Subsystems.SwerveSubsystem;
 import frc.robot.util.Constants.LimelightConstants;
 
+import java.util.Optional;
 import java.util.function.Supplier;
 
 
@@ -20,17 +23,30 @@ public class LimelightPose
     static String LL1 = LimelightConstants.limelight_1_name;
     static String LL2 = LimelightConstants.limelight_2_name;
 
+
+
     public LimelightPose()
     {
 
     }
-    public static void evaluate(SwerveDrivePoseEstimator poseEstimator, ChassisSpeeds speeds)
+    public static void evaluate(SwerveDrivePoseEstimator poseEstimator, ChassisSpeeds speeds, Field2d field)
     {
-        evaluate_single_limelight(LL1, poseEstimator, speeds);
-        evaluate_single_limelight(LL2, poseEstimator, speeds);
+        /*
+        This snippet of code looks scary but is actually pretty nice
+        An Optional<T> object holds a value of type T that may or may not exist, while avoiding null pointer exceptions
+        In this case the evaluate limelight returns an Optional<Pose2d> because it may not see any targets
+
+        One method of an optional is Optional<T>.ifPresent(Consumer<T>)
+        If the optional is present, the Consumer function is run and is passed the value of the optional
+        Since we know the type T, whatever parameter I use is assumed to be that type. So I can make a lambda with (p) -> and it is known to be the Pose2d
+        To add a pose with a name to a Field2d we do field.getObject(String "name").setPose(Pose2d pose)
+        */
+        evaluate_single_limelight(LL1, poseEstimator, speeds).ifPresent((p) -> field.getObject(LL1 + "_pose").setPose(p));
+        evaluate_single_limelight(LL2, poseEstimator, speeds).ifPresent((p) -> field.getObject(LL2 + "_pose").setPose(p));
+
     }
 
-    private static void evaluate_single_limelight(String LL, SwerveDrivePoseEstimator poseEstimator, ChassisSpeeds speeds)
+    private static Optional<Pose2d> evaluate_single_limelight(String LL, SwerveDrivePoseEstimator poseEstimator, ChassisSpeeds speeds)
     {
         LimelightHelpers.SetRobotOrientation(LL, poseEstimator.getEstimatedPosition().getRotation().getDegrees(), 0, 0, 0, 0, 0);
 
@@ -43,7 +59,7 @@ public class LimelightPose
 
         if(poseDiff > 1 || mt2.tagCount == 0 || Math.abs(speeds.omegaRadiansPerSecond) > Units.degreesToRadians(180))
         {
-            return;
+            return Optional.empty();
         }
 
         if(mt2.avgTagDist < 4)
@@ -61,6 +77,8 @@ public class LimelightPose
             mt2.pose,
             mt2.timestampSeconds,
             VecBuilder.fill(stdDev,stdDev,9999999));
+        
+        return Optional.of(mt2.pose);
     }
 
 }
